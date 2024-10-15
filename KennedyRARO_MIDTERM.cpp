3cc43e7da2a4ac79fb32bcaf665bebf55f813ed4
Kenn <kennedyraro79@gmail.com>
2024-10-15 10:09:46 +0800

    Add files via upload

---
+++ KennedyRARO_MIDTERM.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int SugarPriceUSD,ricePricePound,sardinesPricePound,coffeePriceUSD, milkPriceUSD;
+	int sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+	int sugarcostUSD, ricecostPound, sardinescostPound, coffeecostUSD, milkcostUSD;
+	
+	cout << "sugar,rice,sardines,coffee,milk";
+	
+	cout << "sugar cost in USD:";
+	cin >> sugarQty;
+	cout << "rice cost in Pound:";
+	cin >> riceQty;
+	cout << "sardines cost in USD:";
+	cin >> sardinesQty;
+	cout << "coffee cost in Pound:";
+	cin >> coffeeQty;
+	cout << "milk cost in USD:";
+	cin >> milkQty;
+
+	sugarCost = sugarUSD * sugarCost;
+	riceCost = ricePOUND * riceCost;
+	sardinesCost = sardinesUSD * sardinesCost;
+	coffeeCost = coffeePOUND * coffeeCost;
+	milkCost = milkUSD * milkCost;
+	return 0;
+}
